ce9fab451cf236624d3824ae5147f376dc1f465a
Hung Do <17020783@vnu.edu.vn>
2019-04-30 11:35:24 +0700

    dotenv

--- src/routes/googleAuth.js
+++ src/routes/googleAuth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = new express.Router()
 const passport = require('passport')
 const {sendSuccess, sendError} = require('../utils/sendResponse')
+require('dotenv').config()
 
 router.get(
     '/auth/google',
@@ -18,7 +19,10 @@ router.get('/auth/google/callback', (req, res, next) => {
                     return sendError(res, message)
                 }
                 const token = await user.generateToken()
-                const devURL = process.env.ENV === 'heroku' ? `https://clientlivs.herokuapp.com/auth/google/callback/success?token=${token}` : `http://localhost:3000/auth/google/callback/success?token=${token}` 
+                const devURL =
+                    process.env.ENV === 'heroku'
+                        ? `https://clientlivs.herokuapp.com/auth/google/callback/success?token=${token}`
+                        : `http://localhost:3000/auth/google/callback/success?token=${token}`
                 const url =
                     process.env.NODE_ENV === 'production'
                         ? `/auth/google/callback/success?token=${token}`
